9437f6b329adb9f7e476cc3292dfccac762b735a
Ganesh3101 <lordganesh2000@gmail.com>
2023-04-11 11:40:02 +0530

    updated

---
+++ app/src/main/java/com/test/placement_major_project/main_codes/Comps.kt
@@ -0,0 +1,11 @@
+package com.test.placement_major_project.main_codes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Comps : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comps)
+    }
+}
